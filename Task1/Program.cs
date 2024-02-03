00b7940189ee829c8f8314c234f3c8533867fed1
Monya <isadom@mail.ru>
2024-02-03 17:56:22 +0400

    Добавил переменные

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World! Попытка №3");
+﻿// Console.WriteLine("Hello, World! Попытка №3");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d= 5.55;
+char c = '!';
+bool b= true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
