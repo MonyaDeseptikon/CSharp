f3cbb13989e0170cdc597c2c8378f2e298f39d4e
Monya <isadom@mail.ru>
2024-02-24 14:22:34 +0400

    Добавлена рекурсия

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,12 +1,28 @@
-﻿int N = 100;
-int i = 1;
-while (i <= N)
+﻿// int N = 100;
+// int i = 1;
+// while (i <= N)
+// {
+//     Console.Write(i);
+//     Console.Write(' ');
+//     i = i + 1;
+// }
+
+
+/*РЕКУРСИЯ*/
+void openMatryoshka (int size)
 {
-    Console.Write(i);
-    Console.Write(' ');
-    i = i + 1;
+    if (size ==1)
+    {
+        Console.WriteLine ($"Достигнут базовый случай {size}");
+        return;
+    }
+    Console.WriteLine($"Открытый размер матрешки {size}...");
+    openMatryoshka (size-1);
+    Console.WriteLine($"Раскрутка рекурсии {size}...");
+    
 }
 
+openMatryoshka (5);
 
 
 
